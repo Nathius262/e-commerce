936c83a72f52eaa5a25e32010028761e8d93753f
Nathius262 <egbodonathaniel@yahoo.com>
2024-09-24 15:56:56 +0100

    created cart model

--- models/Cart.js
+++ models/Cart.js
@@ -1,25 +1,23 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/database.js';
-import User from './User.js'; // Import User
-import Product from './Product.js'; // Import Product
-
-const Cart = sequelize.define('Cart', 
-    {
-        quantity: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            defaultValue: 1,
-        },
-    }, {
-        timestamps: true,   
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cart extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
-);
-
-// Association: A cart item belongs to a user and a product
-Cart.belongsTo(User, { foreignKey: 'userId' });
-Cart.belongsTo(Product, { foreignKey: 'productId' });
-
-User.hasMany(Cart, { foreignKey: 'userId' });
-Product.hasMany(Cart, { foreignKey: 'productId' });
-
-export default Cart;
+  }
+  Cart.init({
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Cart',
+  });
+  return Cart;
+};
