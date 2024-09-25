889bf90cf904463f3efd232b910a9427c3b821b0
Nathius262 <egbodonathaniel@yahoo.com>
2024-09-25 08:32:01 +0100

    updated package json

--- sync.js
+++ sync.js
@@ -1,6 +1,3 @@
-import sequelize from './config/database.js';
-//import { User, Role, Product, Category, Order, Cart } from './models/index.js';
-
 async function syncDatabase() {
     try {
         await sequelize.sync({ alter:true }); // force: true will drop tables if they exist
