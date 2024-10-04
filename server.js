415e1063d84fad68850f727e0cc3c505f45044f8
Nathius262 <egbodonathaniel@yahoo.com>
2024-10-04 08:54:08 +0100

    modified server.js

--- server.js
+++ server.js
@@ -12,11 +12,16 @@ import {internalServerError, pageNotFound} from './middlewares/errorHandler.js'
 import staticFiles from "./config/staticFiles.js"
 import hbs from "./config/settings.js"
 import removeTrailingSlash  from './middlewares/normalizer.js';
-import sequelize from './config/database.js';
-import './sync.js'; // Sync database
+import db from './models/index.cjs'
+import { createRequire } from 'module';
+const require = createRequire
+
+import adminSeeder  from './seeders/admin-seeder.cjs'; 
 
 //route import
+import adminRouter from './routers/admin/adminRouter.js';
 import rootRouter from './routers/rootRouter.js';
+
 
 dotenv.config()
 
@@ -44,6 +49,7 @@ app.use(staticFiles);
 
 
 app.use('/', rootRouter);
+app.use('/admin/', adminRouter);
 
 
 //middlewares\
@@ -51,24 +57,19 @@ app.use(pageNotFound);
 app.use(internalServerError);
 
 
-
-// Start the server
-const startServer = async () => {
+async function initializeAdmin() {
   try {
-    // Sync database
-    await sequelize.sync({ alter: true });
-
-    // Optionally seed the database
-      // await seedDatabase(); // Uncomment if seeding is needed on every start
-
-      // Start the server
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
-
+    // Ensure admin user is seeded
+    await adminSeeder.seedAdmin(db.sequelize.getQueryInterface(), db.Sequelize);
   } catch (error) {
-      console.error('Error starting server:', error);
+    console.error('Error while initializing admin user:', error);
   }
-};
+}
 
-startServer();
+// Sync database and run seeder if necessary
+//initializeAdmin(); // Ensure the admin seeder runs
+
+  // Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
