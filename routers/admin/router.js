26704de48beb99148d59870747ac45d214aff656
Nathaniel Odo <egbodonathaniel@yahoo.com>
2025-01-17 04:57:56 +0100

    updated routers

---
+++ routers/admin/router.js
@@ -0,0 +1,69 @@
+import { Router } from 'express';
+
+
+import { adminAuthMiddleware } from '../../middlewares/authMiddleware.js';
+import {renderloginAdmin, loginAdmin} from '../../controllers/authController.js'
+import {renderAdminDashboard} from '../../controllers/admin/rootController.js';
+import  * as user from '../../controllers/admin/userController.js';
+import * as role from '../../controllers/admin/roleController.js';
+
+
+const router = Router()
+
+
+//////////////////////
+//////////////////////
+//// USER ROUTER /////
+//////////////////////
+//////////////////////
+
+router.route('/login')
+    .get(renderloginAdmin)
+    .post(loginAdmin);
+
+router.use(adminAuthMiddleware)
+
+
+//////////////////////
+//////////////////////
+//ROOT ADMIN ROUTER //
+//////////////////////
+//////////////////////
+router.get('/', renderAdminDashboard)
+
+
+//////////////////////
+//////////////////////
+//// USER ROUTER /////
+//////////////////////
+//////////////////////
+
+router.route('/user')
+    .get(user.getAllUsers)
+    .post(user.createUser);
+
+router.get('/user/create', user.renderNewUserTemplate);
+
+router.route('/user/:id')
+    .get(user.getUserById)
+    .put(user.updateUserById)
+    .delete(user.deleteUser);
+
+//////////////////////
+//////////////////////
+//// ROLE ROUTER /////
+//////////////////////
+//////////////////////
+
+router.route('/role')
+    .get(role.getAllRoles)
+    .post(role.createRole);
+
+router.get('/role/create', role.renderRoleForm);
+    
+router.route('/role/:id')
+    .get(role.getRoleById)
+    .put(role.updateRole)
+    .delete(role.deleteRole);
+
+export default router;
