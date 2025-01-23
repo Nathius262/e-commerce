729366a27e2f2ba09cdf792d4ad6286643efed68
Nathaniel Odo <egbodonathaniel@yahoo.com>
2025-01-23 23:30:34 +0100

    category helper

---
+++ helpers/categoryHelper.js
@@ -0,0 +1,25 @@
+import db from '../models/index.cjs';
+
+export const getAllCategories = async (page = 1, limit = 10) => {
+  try {
+    const offset = (page - 1) * limit;
+    const { rows: categories, count: totalItems } = await db.Category.findAndCountAll({
+      limit,
+      offset,
+    });
+
+    return {
+      categories: categories.map(category => category.get({ plain: true })),
+      totalItems,
+      totalPages: Math.ceil(totalItems / limit),
+      currentPage: page,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Create a new category
+export async function createCategory({name, description}) {
+  return await db.Category.create({name, description});
+}
